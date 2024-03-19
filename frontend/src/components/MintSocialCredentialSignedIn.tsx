2a7865521879f5dc30853f806897b01aa4756c6f
jhuhnke <jessica@flipsidecrypto.com>
2024-03-19 08:08:43 -0500

    social signed in

--- frontend/src/components/MintSocialCredentialSignedIn.tsx
+++ frontend/src/components/MintSocialCredentialSignedIn.tsx
@@ -133,18 +133,20 @@ const MintSocialCredentialSignedIn: FC = () => {
         <div className='signed-in-wrapper'>
             <NavBar />
             <div className="mint-container">
-                <h1>Logged In Success</h1>
-                <div>
-                    <p>Logged in with: {user?.app_metadata.provider.toUpperCase()}</p>
-                    <p>Username: {user?.user_metadata.full_name || user?.user_metadata.preferred_username || 'Unknown'}</p>
-                    <p>User ID: {user?.identities?.[0]?.id || 'Unknown ID'}</p>
-                    {user?.user_metadata.avatar_url && (
-                        <img src={user.user_metadata.avatar_url} alt="Profile" style={{ height: 100 }} />
-                    )}
-                </div>
-                <div className="button-container">
-                    <button onClick={handleMint} className="mint-button">Mint Credential</button>
-                    <button onClick={signOutUser} className="sign-out-button">Sign Out</button>
+                <div className='signed-in-card'>
+                    <h1>Logged In Success</h1>
+                    <div>
+                        <p>Logged in with: {user?.app_metadata.provider.toUpperCase()}</p>
+                        <p>Username: {user?.user_metadata.full_name || user?.user_metadata.preferred_username || 'Unknown'}</p>
+                        <p>User ID: {user?.identities?.[0]?.id || 'Unknown ID'}</p>
+                        {user?.user_metadata.avatar_url && (
+                            <img src={user.user_metadata.avatar_url} alt="Profile" className='pfp' style={{ height: 100 }} />
+                        )}
+                    </div>
+                    <div className="button-container">
+                        <button onClick={handleMint} className="signed-in-button">Mint Credential</button>
+                        <button onClick={signOutUser} className="signed-in-button">Sign Out</button>
+                    </div>
                 </div>
             </div>
             <Footer />
